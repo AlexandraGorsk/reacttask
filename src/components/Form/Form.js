bad682e1f8f233afac1fa0fce1657f0e76b40499
Alexandra <alex_gorskaya1@mail.ru>
2022-01-16 21:21:31 +0300

    form updated

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Form() {
 	const [formData, setFormData] = useState({
@@ -10,12 +10,39 @@ function Form() {
 		lastName: '',
 		address: '',
 		phone: '',
+		important: 'yes',
 	});
+	const [errorLogin, setErrorLogin] = useState('');
+	// const [errorValidateEmail, setErrorValidateEmail] = useState('');
+	const [touchedLogin, setTouchedLogin] = useState(false);
+	const [formValid, setFormValid] = useState(false);
 	const handleChangeFormData = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
+	const handleBlurLogin = (e) => {
+		setTouchedLogin(true);
+		if (touchedLogin) {
+			setErrorLogin('Логин не должен быть пустым');
+		}
+	};
+	useEffect(() => {
+		if (errorLogin) {
+			setFormValid(false);
+		} else setFormValid(true);
+	}, [errorLogin]);
+	// function validateEmail(email) {
+	// 	var re =
+	// 		/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+	// 	return re.test(String(email).toLowerCase());
+	// }
 	const sendButton = (e) => {
 		e.preventDefault();
+
+		// if (formData.email !== '') {
+		// 	if (!validateEmail(formData.email)) {
+		// 		setErrorValidateEmail('Имейл не корректный, будь в курсе');
+		// 	}
+		// }
 		console.log(formData);
 		setFormData({
 			gender: 'male',
@@ -25,120 +52,149 @@ function Form() {
 			lastName: '',
 			address: '',
 			phone: '',
+			important: 'yes',
 		});
-		const Checkbox = ({ label, value, onChange }) => {
-			return (
-				<label>
-					<input type='checkbox' checked={value} onChange={onChange} />
-					{label}
-				</label>
-			);
-		};
+	};
+	const RadioGender = ({ label, value, onChange, name }) => {
 		return (
-			<div className='registration'>
-				<form>
-					<div>
-						<Checkbox
-							label='Male'
-							value={formData.gender}
-							onChange={handleChangeFormData}
-						/>
-						<Checkbox
-							label='Female'
-							value={formData.gender}
-							onChange={handleChangeFormData}
-						/>
-					</div>
-					<div>
-						<h4>Login</h4>
-						<input
-							className='input'
-							value={formData.login}
-							name='login'
-							type='text'
-							placeholder='Enter your login'
-							onChange={handleChangeFormData}
-						/>
-					</div>
-					<div>
-						<h4>E-mail</h4>
-						<input
-							className='input'
-							value={formData.email}
-							name='email'
-							type='text'
-							placeholder='Enter your email'
-							onChange={handleChangeFormData}
-						/>
-					</div>
-					<div>
-						<h4>First Name</h4>
-						{/* <h5 className='errormessage'>{errorEmptyName}</h5> */}
-						<input
-							className='input'
-							value={formData.firstName}
-							name='firstName'
-							type='text'
-							placeholder='Enter your First Name'
-							onChange={handleChangeFormData}
-						/>
-					</div>
-					<div>
-						<h4>Last Name</h4>
-						<input
-							className='input'
-							value={formData.lastName}
-							name='lastName'
-							type='text'
-							placeholder='Enter your Last Name'
-							onChange={handleChangeFormData}
-						/>
-					</div>
-					<div>
-						<h4>Adress</h4>
-						<input
-							className='input'
-							value={formData.adress}
-							name='adress'
-							type='text'
-							placeholder='Enter your Adress'
-							onChange={handleChangeFormData}
-						/>
-					</div>
-					<div>
-						<h4>Mobile Phone</h4>
-						{/* <h5 className='errormessage'>{errorEmptyPhone}</h5> */}
-						<input
-							className='input'
-							value={formData.mobilePhone}
-							name='mobilePhone'
-							type='text'
-							placeholder='Enter your mobile phone'
-							onChange={handleChangeFormData}
-						/>
-					</div>
-					{/* <div>
-					<h4>Receive important messages</h4>
-					<Checkbox
-						label='Yes'
-						value={formData.importantYes}
-						onChange={handleImportantYes}
-					/>
-					<Checkbox
-						label='No'
-						value={importantNo}
-						onChange={handleImportantNo}
-					/>
-				</div> */}
-					<div>
-						<button className='send' onClick={sendButton}>
-							Send
-						</button>
-					</div>
-				</form>
-			</div>
+			<label>
+				<input
+					name={name}
+					type='radio'
+					checked={value === formData.gender}
+					value={value}
+					onChange={onChange}
+				/>
+				{label}
+			</label>
 		);
 	};
+	const RadioMessage = ({ label, value, onChange, name }) => {
+		return (
+			<label>
+				<input
+					name={name}
+					type='radio'
+					checked={value === formData.important}
+					value={value}
+					onChange={onChange}
+				/>
+				{label}
+			</label>
+		);
+	};
+	return (
+		<div className='registration'>
+			<form>
+				<div>
+					<RadioGender
+						label='Male'
+						name='gender'
+						value='male'
+						onChange={handleChangeFormData}
+					/>
+					<RadioGender
+						label='Female'
+						name='gender'
+						value='Female'
+						onChange={handleChangeFormData}
+					/>
+				</div>
+				<div>
+					<h4>Login</h4>
+					{touchedLogin && errorLogin && (
+						<h5 className='errormessage'>{errorLogin}</h5>
+					)}
+					<input
+						className='input'
+						value={formData.login}
+						name='login'
+						type='text'
+						placeholder='Enter your login'
+						onChange={handleChangeFormData}
+						onBlur={handleBlurLogin}
+						required
+					/>
+				</div>
+				<div>
+					<h4>E-mail</h4>
+					{/* <h5 className='errormessage'>{errorValidateEmail}</h5> */}
+					<input
+						className='input'
+						value={formData.email}
+						name='email'
+						type='text'
+						placeholder='Enter your email'
+						onChange={handleChangeFormData}
+					/>
+				</div>
+				<div>
+					<h4>First Name</h4>
+					<input
+						className='input'
+						value={formData.firstName}
+						name='firstName'
+						type='text'
+						placeholder='Enter your First Name'
+						onChange={handleChangeFormData}
+					/>
+				</div>
+				<div>
+					<h4>Last Name</h4>
+					<input
+						className='input'
+						value={formData.lastName}
+						name='lastName'
+						type='text'
+						placeholder='Enter your Last Name'
+						onChange={handleChangeFormData}
+					/>
+				</div>
+				<div>
+					<h4>Adress</h4>
+					<input
+						className='input'
+						value={formData.address}
+						name='address'
+						type='text'
+						placeholder='Enter your Adress'
+						onChange={handleChangeFormData}
+					/>
+				</div>
+				<div>
+					<h4>Mobile Phone</h4>
+					<input
+						className='input'
+						value={formData.phone}
+						name='phone'
+						type='text'
+						placeholder='Enter your mobile phone'
+						onChange={handleChangeFormData}
+					/>
+				</div>
+				<div>
+					<h4>Receive important messages</h4>
+					<RadioMessage
+						label='YES'
+						name='important'
+						value='yes'
+						onChange={handleChangeFormData}
+					/>
+					<RadioMessage
+						label='NO'
+						name='important'
+						value='no'
+						onChange={handleChangeFormData}
+					/>
+				</div>
+				<div>
+					<button disabled={!formValid} className='send' onClick={sendButton}>
+						Send
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
 
 export default Form;
